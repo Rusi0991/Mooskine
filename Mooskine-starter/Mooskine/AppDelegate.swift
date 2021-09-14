054df30d4d844af2209872d41feb2867aadc0798
Rusi0991 <ruslanismayilov484@gmail.com>
2021-09-13 17:20:16 -0700

    extension DataController for autosaving

--- Mooskine-starter/Mooskine/AppDelegate.swift
+++ Mooskine-starter/Mooskine/AppDelegate.swift
@@ -30,6 +30,7 @@ let dataController = Datacontroller(modelName: "Mooskine")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        saveViewcontext()
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -43,9 +44,12 @@ let dataController = Datacontroller(modelName: "Mooskine")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+        saveViewcontext()
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    func saveViewcontext(){
+        try? dataController.viewContext.save()
+    }
 
 }
 
--- Mooskine-starter/Mooskine/Model/DataController.swift
+++ Mooskine-starter/Mooskine/Model/DataController.swift
@@ -27,7 +27,25 @@ class Datacontroller {
             guard error == nil else {
                 fatalError(error!.localizedDescription)
             }
+            self.autoSaveViewContext()
             completion?()
         }
+    }
+}
+
+extension Datacontroller{
+    func autoSaveViewContext(interval : TimeInterval = 30){
+        print("autosaving")
+        guard interval > 0 else {
+            print("cannot save the negative interval")
+            return
+        }
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval : interval)
+        }
+        
     }
 }
